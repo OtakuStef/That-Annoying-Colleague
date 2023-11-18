1a648011be5ebad90089e407ce9658dddcfcd88e
Stefan <stefanpfleger@rocketmail.com>
2023-11-18 20:26:28 +0100

    Object Duarability
    Added Object Manager, Script for Durability, Object Destruction on damage, Initial Damage Calculation for Object vs Object and Player vs. Object

--- Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -100,7 +100,7 @@ namespace UnityChan
             {   // スペースキーを入力したら
 
                 //アニメーションのステートがLocomotionの最中のみジャンプできる
-                if (currentBaseState.nameHash == locoState)
+                if (currentBaseState.fullPathHash == locoState)
                 {
                     //ステート遷移中でなかったらジャンプできる
                     if (!anim.IsInTransition(0))
@@ -122,7 +122,7 @@ namespace UnityChan
             // 以下、Animatorの各ステート中での処理
             // Locomotion中
             // 現在のベースレイヤーがlocoStateの時
-            if (currentBaseState.nameHash == locoState)
+            if (currentBaseState.fullPathHash == locoState)
             {
                 //カーブでコライダ調整をしている時は、念のためにリセットする
                 if (useCurves)
@@ -132,7 +132,7 @@ namespace UnityChan
             }
             // JUMP中の処理
             // 現在のベースレイヤーがjumpStateの時
-            else if (currentBaseState.nameHash == jumpState)
+            else if (currentBaseState.fullPathHash == jumpState)
             {
                 cameraObject.SendMessage("setCameraPositionJumpView");  // ジャンプ中のカメラに変更
                                                                         // ステートがトランジション中でない場合
@@ -175,7 +175,7 @@ namespace UnityChan
             }
             // IDLE中の処理
             // 現在のベースレイヤーがidleStateの時
-            else if (currentBaseState.nameHash == idleState)
+            else if (currentBaseState.fullPathHash == idleState)
             {
                 //カーブでコライダ調整をしている時は、念のためにリセットする
                 if (useCurves)
@@ -190,7 +190,7 @@ namespace UnityChan
             }
             // REST中の処理
             // 現在のベースレイヤーがrestStateの時
-            else if (currentBaseState.nameHash == restState)
+            else if (currentBaseState.fullPathHash == restState)
             {
                 //cameraObject.SendMessage("setCameraPositionFrontView");		// カメラを正面に切り替える
                 // ステートが遷移中でない場合、Rest bool値をリセットする（ループしないようにする）
