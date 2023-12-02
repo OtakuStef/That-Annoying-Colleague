ae548ad6242da600d18e7e37b4cdc095bbbba0b2
noproblemdude <leonardrolandvictor@googlemail.com>
2023-12-02 16:56:15 +0100

    Splitscreen implemented and UIManager

---
+++ Assets/UIPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIPlayer : MonoBehaviour
+{
+
+    [SerializeField] HealthBar healthBar;
+
+    [SerializeField] EnergyBar energyBar;
+    [SerializeField] PowerUpUI powerUp;
+    [SerializeField] TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
