8cd50ffb374838e636b9fd182b7e3dc6f0a4a91f
Timur I <117691951+timidcampus@users.noreply.github.com>
2023-12-03 15:48:35 +0100

    SFX settings added

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -8,14 +8,22 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
 
     public void Start()
     {
         SetMusicVolume();
+        SetSFXVolume();
     }
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
     }
 }
